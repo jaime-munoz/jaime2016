f135f8070e548026e493f653d0368a47a67dadfd
Jaime Muñoz Rodríguez <jaime.munoz.rodriguez@alumnos.upm.es>
2016-05-04 13:04:27 +0200

    Despliegue DB en Heroku

---
+++ models/index.js
@@ -0,0 +1,40 @@
+var path = require('path');
+
+var Sequelize = require('sequelize');
+
+var url, storage;
+
+if (!process.env.DATABASE_URL){
+  url = "sqlite:///";
+} else {
+  url = process.env.DATABASE_URL;
+  storage = process.env.DATABASE_STORAGE || "";
+}
+var sequelize = new Sequelize(url,
+                              { storage: storage,
+                                omitNull: true
+                              });
+var Quiz = sequelize.import(path.join(__dirname,'quiz'));
+
+sequelize
+.sync()
+.then(function() {
+  return
+    Quiz
+      .count()
+      .then(function (c) {
+        if (c==0){
+          return
+            Quiz
+            .create({ question: 'Capital de Italia', answer: 'Roma' })
+            .then(function() {
+              console.log('Base de datos inicializada con datos');
+            });
+        }
+      });
+}).catch(function(error) {
+    console.log("Error Sincronizando las tablas de la BBDD:",error);
+    process.exit(1);
+});
+
+exports.Quiz = Quiz;
