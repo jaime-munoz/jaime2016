e0cf5bebed9012d513a4e10a791d3650387cd389
Jaime Muñoz Rodríguez <jaime.munoz.rodriguez@alumnos.upm.es>
2016-05-04 18:10:23 +0200

    P10

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,15 +1,37 @@
 var models = require('../models');
 
-//GET /question
-exports.question =function (req, res, next){
+exports.load = function(req, res, next, quizId) {
+  models.Quiz.findById(quizId)
+    .then(function(quiz) {
+      if (quiz) {
+        req.quiz = quiz;
+        next();
+      } else {
+        next(new Error('No existe quizId=' + quizId));
+      }
+    }).catch(function(error) { next(error); });
+};
+
+//GET /quizzes
+exports.index = function(req, res, next) {
   models
   .Quiz
-  .findOne()
+  .findAll()
+  .then(function(quizzes) {
+    res.render('quizzes/index.ejs', { quizzes: quizzes});
+  })
+  .catch(function(error) { next(error); });
+};
+//GET /question
+exports.show =function (req, res, next){
+  models
+  .Quiz
+  .findById(req.params.quizId)
   .then(function(quiz) {
     if (quiz) {
       var answer = req.query.answer || '';
       res
-      .render('quizzes/question', {question: quiz.question, answer: answer});
+      .render('quizzes/show', {quiz: quiz, answer: answer});
     }else{
       throw new Error('No hay preguntas en la BBDD.');
     }
@@ -20,15 +42,89 @@ exports.question =function (req, res, next){
 exports.check=function(req,res,next){
   models
   .Quiz
-  .findOne()
+  .findById(req.params.quizId)
   .then(function(quiz) {
     if(quiz) {
       var answer = req.query.answer || "";
   var result= ((answer=== 'Roma') ? 'Correcta' : 'Incorrecta');
-  res.render('quizzes/result',{result:result, answer: answer});
+  res.render('quizzes/result',{quiz: quiz, result:result, answer: answer});
     }
     else {
-      throw new Error('No hay preguntas en la BBDD.');
+      throw new Error('No existe ese quiz en la BBDD.');
     }
   }).catch(function(error) { next(error); });
+};
+
+//GET /quizzes/new
+exports.new = function(req, res, next) {
+  var quiz = models.Quiz.build({question: "", answer: ""});
+  res.render('quizzes/new', {quiz: quiz});
+};
+
+//POST /quizzes/create
+exports.create = function(req, res, next) {
+  var quiz = models.Quiz.build({ question: req.body.quiz.question,
+                                  answer:  req.body.quiz.answer} );
+
+quiz.save({fields: ["question", "answer"]})
+    .then(function(quiz) {
+      req.flash('success', 'Quiz creado con éxito.');
+      res.redirect('/quizzes');
+    })
+    .catch(Sequelize.ValidationError, function(error) {
+      
+      req.flash('error', 'Errores en el formulario: ');
+      for (var i in error.errors) {
+        req.flash('error', error.errors[i].value);
+      };
+      
+      res.render('quizzes/new', {quiz: quiz});
+    })
+    .catch(function(error) {
+      req.flash('error', 'Error al crear un Quiz: ' +error.message);
+      next(error);
+    });
+}; 
+
+//GET /quizzes/:id/edit
+exports.edit = function(req, res, next) {
+  var quiz = req.quiz; 
+  res.render('quizzes/edit', {quiz: quiz});
+};
+
+//PUT /quizzes/:id
+exports.update = function(req, res, next) {
+  req.quiz.question = req.body.quiz.question;
+  req.quiz.answer = req.body.quiz.answer;
+  
+  req.quiz.save({fields: ["question", "answer"]})
+  .then(function(quiz) {
+  req.flash('success', 'Quiz editado con éxito.');
+    res.redirect('/quizzes');
+  })
+  .catch(Sequelize.ValidationError, function(error) {
+    
+    req.flash('error', 'Errores en el formulario:');
+    for (var i in error.errors) {
+      req.flash('error', error.errors[i].value);
+    };
+    
+    res.render('quizzes/edit', {quiz: req.quiz});
+  })
+  .catch(function(error) {
+    req.flash('error', 'Error al editar el Quiz: '+error.message);
+      next(error);
+  });
+};
+
+exports.destroy = function(req, res, next) {
+  req.quiz.destroy()
+    .then( function() {
+      req.flash('success', 'Quiz borrado con éxito.');
+      res.redirect('/quizzes');
+    })
+    .catch(function(error){
+      req.flash('error', 'Error al editar el Quiz: '+error.message);
+      next(error);
+    });
 };
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,22 @@
 var express = require('express');
 var router = express.Router();
+
 var quizController = require('../controllers/quiz_controller');
+
 /* GET home page. */
   router.get('/', function(req, res, next) {
     res.render('index', { title: 'Quiz' });
   });
-
-router.get('/question', quizController.question);
-router.get('/check', quizController.check);
+  
+router.param('quizId', quizController.load); 
+  
+router.get('/quizzes',                      quizController.index);
+router.get('/quizzes/:quizId(\\d+)',        quizController.show);
+router.get('/quizzes/:quizId(\\d+)/check',  quizController.check);
+router.get('/quizzes/new',                  quizController.new);
+router.post('/quizzes',                     quizController.create);
+router.get('/quizzes/:quizId(\\d+)/edit',   quizController.edit);
+router.put('/quizzes/:quizId(\\d+)',        quizController.update);
+router.delete('/quizzes/:quizId(\\d+)',     quizController.destroy);
 
 module.exports = router;
